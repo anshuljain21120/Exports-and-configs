efcdfe51a7bbecc1838f32817ed131d89b3ecf1a
Anshul Jain <anshuljain21120@gmail.com>
2020-11-27 13:45:22 +0530

    Added: Bash will now activate conda

--- Ubuntu/Set_anaconda.sh
+++ Ubuntu/Set_anaconda.sh
@@ -1,6 +1,21 @@
 cd
+
+# Below line calls the running script with bash -i to invoke the  
+# interactive environment, which loads the ~/.bashrc file. This 
+# file has the conda activation script in there. If this line is 
+# removed the bash will report error of not finding conda 
+# command anywhere. 
+eval "$(conda shell.bash hook)"
+
+# Following command modifies conda configuration setting which
+# disables the automatic base activation everytime a terminal
+# starts in the system. Default: True
 conda config --set auto_activate_base false
+
+# Refreshes the bash shell as the above conda configuration
+# setting modifies .bashrc file.
 source ~/.bashrc
+
 conda activate base
 # Updating base
 conda install -c anaconda cupy --yes
@@ -10,7 +25,12 @@ conda install py-xgboost-gpu --yes
 conda install -c conda-forge line_profiler --yes
 conda config --add channels conda-forge
 conda update --all --yes
+
 # Updating .condarc
+# .condarc stores the user settings for conda shell. These
+# below lines will add names of the default packages
+# in .condarc so that every new conda env made will have these 
+# packages by default.
 echo -e 'create_default_packages:' >> .condarc
 echo -e '  - pip' >> .condarc
 echo -e '  - notebook' >> .condarc
@@ -27,12 +47,15 @@ echo -e '  - selenium' >> .condarc
 echo -e '  - seaborn' >> .condarc
 echo -e '  - urllib3' >> .condarc
 echo -e '  - plotly' >> .condarc
+
 # Creating Environments
+# These default envs will be created change accordingly.
 conda create -n Main --clone base --yes
 conda create -n PyT --clone Main --yes
 conda create -n TF --clone Main --yes
 conda create -n CV --yes
 conda create -n NLP --clone Main --yes
+
 # Updating Environments
 #	Pytorch
 conda install -n PyT pytorch torchvision --yes
@@ -53,8 +76,9 @@ conda install -n NLP -c conda-forge spacy spacy-lookups-data --yes
 conda update -n NLP --all --yes
 # Updating Conda
 conda update conda --yes
-#
-#
+
+
+# OPTIONAL SETUPS
 # To load %lprun command extension by default in Notebook
 FILE="$(ipython locate)/profile_default/ipython_config.py"
 ipython profile create
@@ -65,4 +89,5 @@ echo -e "    'line_profiler'," >> ipython_config.py
 echo -e ']' >> ipython_config.py
 echo -e '#------------------------------------------------------------------------------' >> ipython_config.py
 cd
+
 conda deactivate 
